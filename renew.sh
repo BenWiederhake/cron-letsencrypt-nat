5f3ea1c0ae0410a547dfff966fc5dc3283037f69
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-11-14 18:18:30 +0100

    Better timing control, disable connectivity check
    My router is weird, and I have several seconds ping between the machine and itself.
    WTF!

--- renew.sh
+++ renew.sh
@@ -25,7 +25,7 @@ then
      # Note that this only goes up to 23 hours, not 24. Thus this invocation
      # is incredibly likely to finish before the script gets called again,
      # even if for some reason that happens already the next day.
-     SLEEP_SECONDS="$(shuf -i 10-82800 -n1)"
+     SLEEP_SECONDS="$(shuf -i 10-3600 -n1)"
      # Don't print SLEEP_SECONDS in advance, just in case.
      sleep "${SLEEP_SECONDS}"
      echo "Slept for ${SLEEP_SECONDS} seconds."
@@ -33,7 +33,7 @@ then
 fi
 
 # Verification itself
-echo "$(date): Invocation"
+echo "$(date): Invocation start"
 ! [ -x ./local_hook_pre_verify ] || ./local_hook_pre_verify
 git diff --exit-code
 HTTPDIR="$(mktemp -d tmp-serve-http-XXXXXXXX)"
@@ -42,10 +42,11 @@ mkdir -p "${HTTPDIR}"/.well-known/acme-challenge/
 touch "${HTTPDIR}"/index.html
 touch "${HTTPDIR}"/.well-known/acme-challenge/index.html
 ! [ -x ./local_hook_tamper_verify ] || ./local_hook_tamper_verify "${HTTPDIR}"
-( /usr/bin/timeout -k 55s 50s python3 -m http.server -d "${HTTPDIR}" "${port}" || true ; rm -rf "${HTTPDIR}" ) &
+( /usr/bin/timeout -k 205s 200s python3 -m http.server -d "${HTTPDIR}" "${port}" || true ; rm -rf "${HTTPDIR}" ) &
 sleep 0.5 # Just in case the python core libs load slower than acme-tiny (!)
-/usr/bin/time acme-tiny --account-key ./account.key --csr ./domain.csr \
+/usr/bin/time acme-tiny --disable-check --account-key ./account.key --csr ./domain.csr \
           --acme-dir "${HTTPDIR}"/.well-known/acme-challenge/ > ./signed_chain.crt
+echo "$(date): Invocation end"
 # The "time" shows you how much slack there is regarding the server timeout.
 ! [ -x ./local_hook_post_verify ] || ./local_hook_post_verify "${HTTPDIR}"
 rm -rf "${HTTPDIR}"
